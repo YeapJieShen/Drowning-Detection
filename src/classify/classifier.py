319e2abd914b8e262e5b08289fe54b7d12d5714b
JieShen <yeapjieshen@hotmail.com>
2025-04-27 19:22:36 +0800

    Updated CNN Evaluation

--- src/classify/classifier.py
+++ src/classify/classifier.py
@@ -1,6 +1,11 @@
 import torch
+from torchvision.datasets import ImageFolder
+from sklearn.metrics import classification_report
+import os
 from pathlib import Path
+from collections import defaultdict
 import json
+from tqdm import tqdm
 import yaml
 from .models import CNNClassifier
 from .trainer import TorchClassifierTrainer
@@ -8,6 +13,7 @@ from .trainer import TorchClassifierTrainer
 MODEL_REGISTRY = {
     'CNNClassifier': CNNClassifier
 }
+
 
 class TorchClassifier(torch.nn.Module):
     def __init__(self, model, model_path=None, config=None, device='auto', verbose=False):
@@ -20,11 +26,12 @@ class TorchClassifier(torch.nn.Module):
         self.config = config
 
         self.verbose = verbose
-        self.device = torch.device(device if device != 'auto' else 'cuda' if torch.cuda.is_available() else 'cpu')
+        self.device = torch.device(
+            device if device != 'auto' else 'cuda' if torch.cuda.is_available() else 'cpu')
 
         self.model = None
         self.trainer = None
-        
+
         if model_path:
             self.load(model_path)
         elif config:
@@ -50,12 +57,13 @@ class TorchClassifier(torch.nn.Module):
     def _load_config(self, config):
         if isinstance(config, dict):
             return config
-        
+
         config_path = Path(config)
 
         if not config_path.exists():
-            raise FileNotFoundError(f"Configuration file {config_path} not found!")
-        
+            raise FileNotFoundError(
+                f"Configuration file {config_path} not found!")
+
         if config_path.suffix == '.json':
             with open(config_path, 'r') as f:
                 config = json.load(f)
@@ -63,19 +71,20 @@ class TorchClassifier(torch.nn.Module):
             with open(config_path, 'r') as f:
                 config = yaml.safe_load(f)
         else:
-            raise ValueError(f"Unsupported configuration file format: {config_path.suffix}")
-            
+            raise ValueError(
+                f"Unsupported configuration file format: {config_path.suffix}")
+
     def load(self, weights):
         if Path(weights).suffix != '.pt':
             raise ValueError(f"Model file must be a .pt file, got {weights}")
-        
+
         checkpoint = torch.load(weights, map_location='cpu')
 
         config = checkpoint.get("config")
 
         if config is None:
             raise ValueError("Missing model configuration in checkpoint!")
-        
+
         self.model = self.model_cls(**config).to(self.device)
         self.model.load_state_dict(checkpoint["model_state_dict"])
         self.model.eval()
@@ -90,6 +99,76 @@ class TorchClassifier(torch.nn.Module):
 
         self.class_to_idx = self.trainer.class_to_idx
         return self.training_logs
+
+    @staticmethod
+    def stratified_subset(imagefolder, fraction=1.0, head=True):
+        grouped = defaultdict(list)
+
+        for img_path, label in imagefolder.imgs:
+            grouped[label].append((img_path, label))
+
+        subset = []
+        for label, samples in grouped.items():
+            k = max(1, int(len(samples) * fraction))
+            if head:
+                subset.extend(samples[:k])
+            else:
+                subset.extend(samples[-k:])
+
+        imagefolder.imgs = subset
+        imagefolder.samples = subset
+        return imagefolder
+    
+    def validate(self, data_path, transform, val_test_ratio):
+        val_dir = os.path.join(data_path, 'val')
+
+        val_dataset = ImageFolder(val_dir, transform=transform)
+        class_labels = val_dataset.classes
+
+        val_dataset = self.stratified_subset(
+            val_dataset, fraction=val_test_ratio, head=False)
+
+        val_loader = torch.utils.data.DataLoader(
+            val_dataset,
+            batch_size=32,
+            shuffle=False
+        )
+
+        self.model.eval()
+
+        all_labels = []
+        all_preds = []
+
+        with torch.no_grad():
+            for batch_idx, (inputs, targets) in tqdm(enumerate(val_loader), total=len(val_loader), desc="Validation"):
+                inputs, targets = inputs.to(self.device), targets.to(self.device)
+
+                outputs = self.model(inputs)
+                _, predicted = torch.max(outputs.data, 1)
+
+                all_labels.extend(targets.cpu().numpy())
+                all_preds.extend(predicted.cpu().numpy())
+
+        print(
+            classification_report(
+                all_labels,
+                all_preds,
+                target_names=class_labels,
+                digits=4,
+                zero_division=0
+            )
+        )
+
+        return {
+            **classification_report(
+                all_labels,
+                all_preds,
+                target_names=class_labels,
+                digits=4,
+                zero_division=0,
+                output_dict=True
+            )
+        }
 
     def predict(self, img, transform=None, prob=False):
         if transform:
@@ -106,7 +185,7 @@ class TorchClassifier(torch.nn.Module):
     def save(self, filename):
         if not filename.endswith('.pt'):
             raise ValueError("Model file must be a .pt file!")
-        
+
         torch.save({
             'model_state_dict': self.model.state_dict(),
             'config': self.config
@@ -114,12 +193,3 @@ class TorchClassifier(torch.nn.Module):
 
         if self.verbose:
             print(f"Model saved to {filename}")
-
-
-
-
-
-
-
-
-
